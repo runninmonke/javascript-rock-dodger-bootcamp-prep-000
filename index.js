23d6548ff3700ef7ad608c8c72b5b983389c367f
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-17 17:30:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function checkCollision(rock) {
     const rockRightEdge = rockLeftEdge + 20
 
     if ((rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
-        (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge) ||
+        (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
         (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge))
     {
         return true
